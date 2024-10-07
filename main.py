108dd0c83a88aa1c2c1abcfab4380694a1319ba2
Shamil <65324697+DeadDreik@users.noreply.github.com>
2024-10-07 20:41:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pprint import pprint
 
-
 class Product:
 
     def __init__(self, name, weight, category):
@@ -8,11 +7,8 @@ class Product:
         self.weight = weight
         self.category = category
 
-
-    def  __str__(self):
+    def __str__(self):
         return f"{self.name}, {self.weight}, {self.category}"
-
-
 
 class Shop:
 
@@ -20,27 +16,29 @@ class Shop:
         self.__file_name = 'products.txt'
 
     def get_products(self):
-        self.file = open(self.__file_name, 'r')
-        return self.file.read()
-        self.file.close()
+        with open(self.__file_name, 'r') as file:
+            products = file.read()
+        return products
 
     def add(self, *products):
-        for i in products:
-            self.file = open(self.__file_name, 'r')
-            if i.name not in self.file.read():
-                self.file = open(self.__file_name, 'a')
-                self.file.write(f'{i}\n')
-                self.file.close()
-            else:
-                print(f'Продукт {i.name} уже есть в магазине')
+        with open(self.__file_name, 'r') as file:
+            existing_products = file.read()
+
+        with open(self.__file_name, 'a') as file:
+            for product in products:
+                if product.name not in existing_products:
+                    file.write(f'{product}\n')
+                else:
+                    print(f'Продукт {product.name} уже есть в магазине')
 
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2)# __str__
+print(p2)  
 
 s1.add(p1, p2, p3)
 
 print(s1.get_products())
+
